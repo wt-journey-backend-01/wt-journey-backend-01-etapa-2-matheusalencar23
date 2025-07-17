72454065ddb7127c70d84a6a24d78925e55764ec
Matheus Alencar <matheusalencar6942@gmail.com>
2025-07-17 15:09:43 -0300

    adicionando dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const casosRouter = require("./routes/casosRoutes");
 
