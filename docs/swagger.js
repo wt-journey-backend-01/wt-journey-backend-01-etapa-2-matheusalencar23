f3d02dcfde3b146c245757b553a7640a5cecc4a5
Matheus Alencar <matheusalencar6942@gmail.com>
2025-07-21 19:48:17 -0300

    inicindo documentação

--- docs/swagger.js
+++ docs/swagger.js
@@ -18,6 +18,157 @@ const swaggerOptions = {
         description: "Servidor de desenvolvimento",
       },
     ],
+    paths: {
+      "/casos": {
+        get: {
+          summary: "Retorna todos os casos",
+          description: "Retorna uma lista de todos os casos disponíveis",
+          tags: ["Casos"],
+          responses: {
+            200: {
+              description: "Lista todos os casos",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      properties: {
+                        id: {
+                          type: "string",
+                          format: "uuid",
+                          example: "a4e517b1-06f0-41d5-b65c-8989cea53db9",
+                        },
+                        titulo: {
+                          type: "string",
+                          example: "Homicídio",
+                        },
+                        descricao: {
+                          type: "string",
+                          example:
+                            "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos",
+                        },
+                        status: {
+                          type: "string",
+                          enum: ["aberto", "fechado"],
+                          example: "aberto",
+                        },
+                        agente_id: {
+                          type: "string",
+                          format: "uuid",
+                          example: "85db22b5-d93f-40f2-aade-229ff6096657",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        post: {
+          summary: "Cria um novo caso",
+          description: "Cria um novo caso com os dados fornecidos",
+          tags: ["Casos"],
+          requestBody: {
+            required: true,
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    titulo: {
+                      type: "string",
+                      example: "Homicídio",
+                    },
+                    descricao: {
+                      type: "string",
+                      example:
+                        "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos",
+                    },
+                    status: {
+                      type: "string",
+                      enum: ["aberto", "fechado"],
+                      example: "aberto",
+                    },
+                    agente_id: {
+                      type: "string",
+                      format: "uuid",
+                      example: "85db22b5-d93f-40f2-aade-229ff6096657",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          responses: {
+            201: {
+              description: "Caso criado com sucesso",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      id: {
+                        type: "string",
+                        format: "uuid",
+                        example: "a4e517b1-06f0-41d5-b65c-8989cea53db9",
+                      },
+                      titulo: {
+                        type: "string",
+                        example: "Homicídio",
+                      },
+                      descricao: {
+                        type: "string",
+                        example:
+                          "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos",
+                      },
+                      status: {
+                        type: "string",
+                        enum: ["aberto", "fechado"],
+                        example: "aberto",
+                      },
+                      agente_id: {
+                        type: "string",
+                        format: "uuid",
+                        example: "85db22b5-d93f-40f2-aade-229ff6096657",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            400: {
+              description: "Dados inválidos",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      status: {
+                        type: "integer",
+                        example: 400,
+                      },
+                      message: {
+                        type: "string",
+                        example: "Dados inválidos",
+                      },
+                      errors: {
+                        type: "string",
+                        example: [
+                          "O status é obrigatório",
+                          'O status deve ser "aberto" ou "solucionado"',
+                        ],
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   apis: ["./routes/*.js"],
 };
--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -17,76 +17,8 @@ router.get(
   casosController.getCasosById
 );
 
-/**
- * @openapi
- * /casos:
- *   get:
- *     summary: Retorna todos os casos
- *     description: Retorna uma lista de todos os casos disponíveis
- *     tags: ["Casos"]
- *     responses:
- *       200:
- *         description: Lista todos os casos
- *         content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *               type: object
- *               properties:
- *                id:
- *                  type: string
- *                  format: uuid
- *                  example: a4e517b1-06f0-41d5-b65c-8989cea53db9
- *                titulo:
- *                  type: string
- *                  example: Homicídio
- *                descricao:
- *                  type: string
- *                  example: Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos
- *                status:
- *                  type: string
- *                  enum: [aberto, fechado]
- *                  example: aberto
- *                agente_id:
- *                  type: string
- *                  format: uuid
- *                  example: 85db22b5-d93f-40f2-aade-229ff6096657
- */
 router.get("/casos", casosController.getAllCasos);
 
-/**
- * @openapi
- * /casos:
- *   post:
- *     summary: Cria um novo caso
- *     requestBody:
- *      required: true
- *     content:
- *      application/json:
- *       schema:
- *        type: object
- *        required:
- *          - titulo
- *          - descricao
- *          - status
- *          - agente_id
- *        properties:
- *          titulo:
- *            type: string
- *          descricao:
- *            type: string
- *        status:
- *          type: string
- *          enum: ["aberto", "solucionado"]
- *     responses:
- *      201:
- *        description: Caso criado com sucesso
- *      400:
- *        description: Erro de validação
- *      500:
- *        description: Erro interno do servidor
- * */
 router.post(
   "/casos",
   casosValidation.createInputValidator(),
