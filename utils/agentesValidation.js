8516d95e0648273886047e6726b87850fd4cf204
Matheus Alencar <matheusalencar6942@gmail.com>
2025-07-22 17:09:08 -0300

    bloquando datas no futuro

--- utils/agentesValidation.js
+++ utils/agentesValidation.js
@@ -1,5 +1,14 @@
 const { body } = require("express-validator");
 
+const futureDateValidation = (value) => {
+  const now = new Date();
+  const inputDate = new Date(value);
+  if (inputDate > now) {
+    throw new Error("A data não pode estar no futuro");
+  }
+  return true;
+};
+
 function createInputValidator() {
   return [
     body("nome").notEmpty().withMessage("O nome é obrigatório"),
@@ -7,7 +16,8 @@ function createInputValidator() {
       .notEmpty()
       .withMessage("A data de incorporação é obrigatória")
       .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("A data de incorporação deve estar no formato YYYY-MM-DD"),
+      .withMessage("A data de incorporação deve estar no formato YYYY-MM-DD")
+      .custom(futureDateValidation),
     body("cargo").notEmpty().withMessage("O cargo é obrigatório"),
   ];
 }
@@ -20,7 +30,8 @@ function createPartialInputValidator() {
       .notEmpty()
       .withMessage("A data de incorporação não pode ser vazia")
       .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("A data de incorporação deve estar no formato YYYY-MM-DD"),
+      .withMessage("A data de incorporação deve estar no formato YYYY-MM-DD")
+      .custom(futureDateValidation),
     body("cargo").optional(),
   ];
 }
